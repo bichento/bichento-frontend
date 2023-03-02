19255f712304e2e9ba5e6e04cb8f2ec5a60da763
tchesa <cesarolimpio@gmail.com>
2023-03-02 17:44:04 -0300

    create layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,50 @@
+import { ReactElement, ReactNode } from "react"
+import { AppBar, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography } from "@mui/material"
+import PetsIcon from '@mui/icons-material/Pets';
+import { AppProps } from "next/app";
+import { NextPage } from "next";
+import { Copyright } from "@mui/icons-material";
+import { Box } from "@mui/system";
+
+const theme = createTheme()
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type Props = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const Layout = ({ Component, pageProps }: Props) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PetsIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Bichento
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Component {...pageProps} />
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+    </ThemeProvider>
+  )
+}
+
+export default Layout
