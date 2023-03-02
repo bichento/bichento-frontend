29f626d227e7fc078b606cfca1b3d9fb6a83ceda
tchesa <cesarolimpio@gmail.com>
2023-03-02 17:25:11 -0300

    update home page

--- src/components/pet-card.tsx
+++ src/components/pet-card.tsx
@@ -1,16 +1,16 @@
 import { Pet } from "@/types"
-import { Card, CardContent, CardMedia, Typography } from "@mui/material"
+import { Button, Card, CardActions, CardContent, SxProps, Typography } from "@mui/material"
+import { Theme } from "@mui/system"
 import Image from "next/image"
-import Link from "next/link"
 
 type Props = {
-  pet: Pet
+  pet: Pet,
+  sx?: SxProps<Theme>
 }
 
-const PetCard = ({ pet }: Props) => (
-  <Link href={`/pets/${pet.sys.id}`}>
-    <Card>
-      {/* <CardMedia sx={{ height: 140 }} image={pet.fields.thumbnail.fields.file.url} title={pet.fields.name} /> */}
+const PetCard = ({ pet, ...cardProps }: Props) => (
+  <Card {...cardProps}>
+    {/* <Card>
       <div style={{ width: 140, height: 140 }}>
         <Image
           src={`https:${pet.fields.thumbnail.fields.file.url}`}
@@ -23,8 +23,34 @@ const PetCard = ({ pet }: Props) => (
       <CardContent>
         <Typography variant="h5" component="div">{pet.fields.name}</Typography>
       </CardContent>
-    </Card>
-  </Link>
+    </Card> */}
+    {/* <CardMedia
+      component="img"
+      sx={{
+        // 16:9
+        pt: '56.25%',
+      }}
+      image="https://source.unsplash.com/random"
+      alt="random"
+    /> */}
+    <Image
+      src={`https:${pet.fields.thumbnail.fields.file.url}`}
+      alt={pet.fields.name}
+      width={pet.fields.thumbnail.fields.file.details.image.width}
+      height={pet.fields.thumbnail.fields.file.details.image.height}
+      style={{ width: '100%', height: 250, objectFit: 'cover' }}
+    // fill
+    />
+    <CardContent sx={{ flexGrow: 1 }}>
+      <Typography gutterBottom variant="h5" component="h2">
+        {pet.fields.name}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" href={`/pets/${pet.sys.id}`}>View</Button>
+      {/* <Button size="small">Edit</Button> */}
+    </CardActions>
+  </Card>
 )
 
 export default PetCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import PetCard from '@/components/pet-card'
 import { Pet } from '@/types'
+import { Box, Button, Container, Grid, Stack, Typography } from '@mui/material'
 import { createClient, Entry } from 'contentful'
 import { GetStaticProps } from 'next'
 
@@ -29,11 +30,54 @@ export default function Home({ pets }: Props) {
   console.log(pets)
 
   return (
-    <div>
-      <h1>Home</h1>
-      {pets.map(pet => (
-        <PetCard key={pet.sys.id} pet={pet} />
-      ))}
-    </div>
+    // <div>
+    //   <h1>Home</h1>
+    //   {pets.map(pet => (
+    //     <PetCard key={pet.sys.id} pet={pet} />
+    //   ))}
+    // </div>
+    <main>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Bichento
+          </Typography>
+          <Typography variant="h5" align="center" color="text.secondary" paragraph>
+            Pets para adoção.
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            {/* <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button> */}
+          </Stack>
+        </Container>
+      </Box>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        {/* End hero unit */}
+        <Grid container spacing={4}>
+          {pets.map((pet) => (
+            <Grid item key={pet.sys.id} xs={12} sm={6} md={4}>
+              <PetCard key={pet.sys.id} pet={pet} sx={{ height: '100%', display: 'flex', flexDirection: 'column' }} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </main>
   )
 }
